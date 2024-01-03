7955e91c1240331d7d7724e815e9b00b3b432ab4
Marlie Denis <101306894+Marlied1@users.noreply.github.com>
2024-01-02 20:15:32 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return ( 
     <footer className='bg-primary py-12'>
       <div className='container mx-auto'>
-        <p className='text-white text-center'> &copy; Copyright 2024 MarlieTechDesings.</p>
+        <p className='text-white text-center'> &copy; Copyright 2024 MarlieTechDesigns.</p>
       </div>
     </footer>
   );
